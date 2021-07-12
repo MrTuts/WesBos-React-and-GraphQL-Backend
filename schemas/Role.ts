6cc01ac22816767f466b156f58fe18b41a97a031
Pavel <pavelgric@gmail.com>
2021-07-12 08:22:33 +0200

    66 - Hide Roles scheme from regular users

--- schemas/Role.ts
+++ schemas/Role.ts
@@ -1,8 +1,21 @@
 import { relationship, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
 import { permissionFields } from './fields';
+import { permissions } from '../access';
 
 export const Role = list({
+  access: {
+    create: permissions.canManageUsers,
+    read: permissions.canManageUsers,
+    update: permissions.canManageUsers,
+    delete: permissions.canManageUsers,
+  },
+  ui: {
+    // hide Roles ui from regular users
+    hideCreate: (args) => !permissions.canManageRoles(args),
+    hideDelete: (args) => !permissions.canManageRoles(args),
+    isHidden: (args) => !permissions.canManageRoles(args),
+  },
   fields: {
     name: text({ isRequired: true }),
     assignedTo: relationship({
